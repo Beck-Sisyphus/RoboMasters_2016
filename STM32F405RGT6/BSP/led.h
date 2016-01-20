fb49aec9055517c5ea2c1d0e52be0cdfad0dc797
Beck Pang <pshfls@gmail.com>
2016-01-19 21:12:16 -0800

    Update to the new board design: LED
    Thanks for the help from Marissa

--- STM32F405RGT6/BSP/led.h
+++ STM32F405RGT6/BSP/led.h
@@ -3,13 +3,13 @@
 
 #include <stm32f4xx.h>
 
-#define LED1_ON()       GPIO_ResetBits(GPIOB,GPIO_Pin_14)
-#define LED1_OFF()      GPIO_SetBits(GPIOB,GPIO_Pin_14)
-#define LED1_TOGGLE()   (GPIOB->ODR) ^= GPIO_Pin_14//RED
+#define LED1_ON()       GPIO_ResetBits(GPIOC,GPIO_Pin_1)
+#define LED1_OFF()      GPIO_SetBits(GPIOC,GPIO_Pin_1)
+#define LED1_TOGGLE()   (GPIOC->ODR) ^= GPIO_Pin_1//RED
 
-#define LED2_ON()       GPIO_ResetBits(GPIOB,GPIO_Pin_15)
-#define LED2_OFF()      GPIO_SetBits(GPIOB,GPIO_Pin_15)
-#define LED2_TOGGLE()   (GPIOB->ODR) ^= GPIO_Pin_15//BLUE
+#define LED2_ON()       GPIO_ResetBits(GPIOC,GPIO_Pin_2)
+#define LED2_OFF()      GPIO_SetBits(GPIOC,GPIO_Pin_2)
+#define LED2_TOGGLE()   (GPIOC->ODR) ^= GPIO_Pin_2//BLUE
 
 
 void LED_Configuration(void);
