35339f122c9aab760145fe18c9e3de96569c57d4
Aotian <aotianzheng@gmail.com>
2016-03-04 19:09:51 -0800

    Aotian pwm

--- STM32F405RGT6/BSP/pwm.c
+++ STM32F405RGT6/BSP/pwm.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/*-LEFT---(PB3---TIM2_CH2)--*/
-/*-RIGHT--(PA15--TIM2_CH1)--*/
+/*-LEFT---(PB3---TIM5_CH2)--*/
+/*-RIGHT--(PA15--TIM5_CH1)--*/
+TIM_OCInitTypeDef         oc;
 
 void PWM_Configuration(void)
 {
@@ -9,47 +10,45 @@ void PWM_Configuration(void)
     TIM_TimeBaseInitTypeDef   tim;
     TIM_OCInitTypeDef         oc;
     
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB,ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA ,ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
 
-    gpio.GPIO_Pin = GPIO_Pin_1;
-    gpio.GPIO_Mode = GPIO_Mode_AF;
-    gpio.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_Init(GPIOA,&gpio);
-    
-    gpio.GPIO_Pin = GPIO_Pin_0;
+    gpio.GPIO_Pin = GPIO_Pin_1|GPIO_Pin_0;
     gpio.GPIO_Mode = GPIO_Mode_AF;
     gpio.GPIO_Speed = GPIO_Speed_100MHz;
     GPIO_Init(GPIOA,&gpio);
 
-    GPIO_PinAFConfig(GPIOA,GPIO_PinSource1, GPIO_AF_TIM2);
-    GPIO_PinAFConfig(GPIOA,GPIO_PinSource0,GPIO_AF_TIM2);    
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource1, GPIO_AF_TIM5);
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource0,GPIO_AF_TIM5);    
     
     tim.TIM_Prescaler = 84-1;
     tim.TIM_CounterMode = TIM_CounterMode_Up;
     tim.TIM_Period = 2500;   //2.5ms
     tim.TIM_ClockDivision = TIM_CKD_DIV1;
-    TIM_TimeBaseInit(TIM2,&tim);
+    TIM_TimeBaseInit(TIM5,&tim);
     
-    oc.TIM_OCMode = TIM_OCMode_PWM2;
+    oc.TIM_OCMode = TIM_OCMode_PWM1;
     oc.TIM_OutputState = TIM_OutputState_Enable;
     oc.TIM_OutputNState = TIM_OutputState_Disable;
-    oc.TIM_Pulse = 0;
-    oc.TIM_OCPolarity = TIM_OCPolarity_Low;
-    oc.TIM_OCNPolarity = TIM_OCPolarity_High;
+    oc.TIM_Pulse = 1000;
+    oc.TIM_OCPolarity = TIM_OCPolarity_High;
+    oc.TIM_OCNPolarity = TIM_OCPolarity_Low;
     oc.TIM_OCIdleState = TIM_OCIdleState_Reset;
     oc.TIM_OCNIdleState = TIM_OCIdleState_Set;
-    TIM_OC1Init(TIM2,&oc);
-    TIM_OC2Init(TIM2,&oc);
+    TIM_OC1Init(TIM5,&oc);
+    TIM_OC2Init(TIM5,&oc);
     
-    TIM_OC1PreloadConfig(TIM2,TIM_OCPreload_Enable);
-    TIM_OC2PreloadConfig(TIM2,TIM_OCPreload_Enable);
+    TIM_OC1PreloadConfig(TIM5,TIM_OCPreload_Enable);
+    TIM_OC2PreloadConfig(TIM5,TIM_OCPreload_Enable);
            
-    TIM_ARRPreloadConfig(TIM2,ENABLE);
+    TIM_ARRPreloadConfig(TIM5,ENABLE);
 
-    TIM_Cmd(TIM2,ENABLE);
+    TIM_Cmd(TIM5,ENABLE);
     
 
+}
+void set(int pulse){
+    oc.TIM_Pulse = pulse;
 }
 
 
