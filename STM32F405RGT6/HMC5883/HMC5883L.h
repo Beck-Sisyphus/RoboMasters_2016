2ff97b8b599ea2b3810eb43864fbf3fb968a4241
Weiyou Dai <daiweiyou16@hotmail.com>
2016-03-09 01:13:03 -0800

    reduced warning by adding void as an argument

--- STM32F405RGT6/HMC5883/HMC5883L.h
+++ STM32F405RGT6/HMC5883/HMC5883L.h
@@ -106,37 +106,37 @@ extern "C" {
 #define TRUE												1
 #define FALSE												0
 
-void HMC5883L_Initialize();
-bool HMC5883L_TestConnection();
+void HMC5883L_Initialize(void);
+bool HMC5883L_TestConnection(void);
 
 // CONFIG_A register
-uint8_t HMC5883L_GetSampleAveraging();
+uint8_t HMC5883L_GetSampleAveraging(void);
 void HMC5883L_SetSampleAveraging(uint8_t averaging);
-uint8_t HMC5883L_GetDataRate();
+uint8_t HMC5883L_GetDataRate(void);
 void HMC5883L_SetDataRate(uint8_t rate);
-uint8_t HMC5883L_GetMeasurementBias();
+uint8_t HMC5883L_GetMeasurementBias(void);
 void HMC5883L_SetMeasurementBias(uint8_t bias);
 
 // CONFIG_B register
-uint8_t HMC5883L_GetGain();
+uint8_t HMC5883L_GetGain(void);
 void HMC5883L_SetGain(uint8_t gain);
 
 // MODE register
-uint8_t HMC5883L_GetMode();
+uint8_t HMC5883L_GetMode(void);
 void HMC5883L_SetMode(uint8_t mode);
 
 // DATA* registers
 void HMC5883L_GetHeading(s16* Mag);
 // STATUS register
-bool HMC5883L_GetLockStatus();
-bool HMC5883L_GetReadyStatus();
+bool HMC5883L_GetLockStatus(void);
+bool HMC5883L_GetReadyStatus(void);
 
 void HMC5883L_WriteBits(uint8_t slaveAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data);
 void HMC5883L_WriteBit(uint8_t slaveAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data);
 void HMC5883L_ReadBits(uint8_t slaveAddr, uint8_t regAddr , uint8_t bitStart, uint8_t length, uint8_t *data);
 void HMC5883L_ReadBit(uint8_t slaveAddr, uint8_t regAddr, uint8_t bitNum, uint8_t *data);
 
-void HMC5883L_I2C_Init();
+void HMC5883L_I2C_Init(void);
 void HMC5883L_I2C_ByteWrite(u8 slaveAddr, u8* pBuffer, u8 WriteAddr);
 void HMC5883L_I2C_BufferRead(u8 slaveAddr,u8* pBuffer, u8 ReadAddr, u16 NumByteToRead);
 
