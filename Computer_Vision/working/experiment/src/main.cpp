660c655aed5c52c662041adc486b038b368a3171
mandary <mandary@uw.edu>
2016-04-18 00:01:25 +0000

    blue detected

--- Computer_Vision/working/experiment/src/main.cpp
+++ Computer_Vision/working/experiment/src/main.cpp
@@ -11,105 +11,113 @@
 
 // ZED
 #include <zed/Camera.hpp>
+
 using namespace cv;
 using namespace std;
 
+
+// Input from keyboard
+char keyboard = ' ';
+
 int main(int argc, char** argv)
 {
-	// Initialize ZED color stream in HD and depth in Performance mode
-	sl::zed::Camera* zed = new sl::zed::Camera(sl::zed::HD720);
-	sl::zed::ERRCODE err = zed->init(sl::zed::MODE::PERFORMANCE, 0, true);
+// Initialize ZED color stream in HD and depth in Performance mode
+sl::zed::Camera* zed = new sl::zed::Camera(sl::zed::HD720);
+sl::zed::ERRCODE err = zed->init(sl::zed::MODE::PERFORMANCE, 0, true);
 
-	// Quit if an error occurred
-	if (err != sl::zed::SUCCESS) {
-		std::cout << "Unable to init the ZED:" << errcode2str(err) << std::endl;
-		delete zed;
-		return 1;
-	}	
+// Quit if an error occurred
+if (err != sl::zed::SUCCESS) {
+std::cout << "Unable to init the ZED:" << errcode2str(err) << std::endl;
+delete zed;
+return 1;
+}
 
-	// Initialize color image and depth
-	int width = zed->getImageSize().width;
-	int height = zed->getImageSize().height;
-	cv::Mat image(height, width, CV_8UC4 ,1);
+// Initialize color image and depth
+int width = zed->getImageSize().width;
+int height = zed->getImageSize().height;
+cv::Mat image(height, width, CV_8UC4,1);
+cv::Mat depth(height, width, CV_8UC4,1);
 
+// Create OpenCV windows
+cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
+cv::namedWindow("Depth", cv::WINDOW_AUTOSIZE);
+namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
-	// Create OpenCV windows
-	//cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
-	//cv::namedWindow("Depth", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+//Blue
+int iLowH = 89;
+int iHighH = 125;
 
-	int iLowR = 220;
-	int iHighR = 255;
+int iLowS = 64;
+int iHighS = 255;
 
-	int iLowG = 0;
-	int iHighG = 50;
+int iLowV = 128;
+int iHighV = 255;
 
-	int iLowB = 0;
-	int iHighB = 50;
+////Create trackbars in "Control" window
+cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+cvCreateTrackbar("HighH", "Control", &iHighH, 179);
 
-	//Create trackbars in "Control" window
-	cvCreateTrackbar("LowR", "Control", &iLowR, 220); //Hue (0 - 179)
-	cvCreateTrackbar("HighR", "Control", &iHighR, 255);
+cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+cvCreateTrackbar("HighS", "Control", &iHighS, 255);
 
-	cvCreateTrackbar("LowG", "Control", &iLowG, 0); //Saturation (0 - 255)
-	cvCreateTrackbar("HighG", "Control", &iHighG, 50);
+cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 
-	cvCreateTrackbar("LowB", "Control", &iLowB, 0); //Value (0 - 255)
-	cvCreateTrackbar("HighB", "Control", &iHighB, 50);
-	// Settings for windows
-	cv::Size displaySize(720, 404);
-	cv::Mat imageDisplay(displaySize, CV_8UC4);
+// Settings for windows
+cv::Size displaySize(720, 404);
+cv::Mat imageDisplay(displaySize, CV_8UC4);
+cv::Mat depthDisplay(displaySize, CV_8UC4);
 
-	// Loop until 'q' is pressed
-	int frame = 0;
-	while (frame < 10000) {
+// Loop until 'q' is pressed
+while (keyboard != 'q') {
 
-	 	// Grab frame and compute depth in FULL sensing mode
-		if (!zed->grab(sl::zed::SENSING_MODE::FULL))
-		{
+ // Grab frame and compute depth in FULL sensing mode
+ if (!zed->grab(sl::zed::SENSING_MODE::FULL))
+ {
 
-			// Retrieve left color image
-			sl::zed::Mat right = zed->retrieveImage(sl::zed::SIDE::RIGHT);
-			memcpy(image.data,right.data,width*height*4*sizeof(uchar));
+ // Retrieve left color image
+ sl::zed::Mat left = zed->retrieveImage(sl::zed::SIDE::LEFT);
+ memcpy(image.data,left.data,width*height*4*sizeof(uchar));
 
-			// Retrieve depth map
-			//sl::zed::Mat depthmap = zed->normalizeMeasure(sl::zed::MEASURE::DEPTH);
-			//memcpy(depth.data,depthmap.data,width*height*4*sizeof(uchar));
-
-
-			cv::Mat imgHSV;
-			//Convert the captured frame from BGR to HSV
-			cv::cvtColor(image, imgHSV, CV_BGRA2RGB); 
+// Retrieve depth map
+ sl::zed::Mat depthmap = zed->normalizeMeasure(sl::zed::MEASURE::DEPTH);
+ memcpy(depth.data,depthmap.data,width*height*4*sizeof(uchar));
 
 
-			cv::Mat imgThresholded;
 
-			cv::inRange(imgHSV, cv::Scalar(iLowR, iLowG, iLowB), cv::Scalar(iHighR, iHighG, iHighB), imgThresholded); //Threshold the image
+ Mat imgHSV;
 
-			/*for(int i = 0; i < width*height*3; i++){
-				
-				printf("%d ", imgThresholded.data[i]);
-							
-			}*/
-			//morphological opening (remove small objects from the foreground)
-			cv::erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, cv::Size(5, 5)) );
-			cv::dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, cv::Size(5, 5)) );
+cvtColor(image, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
 
-			//morphological closing (fill small holes in the foreground)
-			cv::dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, cv::Size(5, 5)) );
-			cv::erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, cv::Size(5, 5)) );
-			
-			cv::resize(imgThresholded, imageDisplay, displaySize);
-			cv::imshow("Thresholded Image", imgThresholded); //show the thresholded image
-			cv::resize(image, imageDisplay, displaySize);
-			cv::imshow("Original", image); //show the original image
-			frame++;
-		}
+Mat imgThresholded;
 
-	}
-	
+inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
 
-	delete zed;
-	return 0;
+//morphological opening (remove small objects from the foreground)
+erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+//morphological closing (fill small holes in the foreground)
+dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+imshow("Thresholded Image", imgThresholded); //show the thresholded image
+//imshow("Original", image); //show the original image
+
+
+ // Display image in OpenCV window
+ cv::resize(image, imageDisplay, displaySize);
+ cv::imshow("Image", imageDisplay);
+
+ // Display depth map in OpenCV window
+ cv::resize(depth, depthDisplay, displaySize);
+ cv::imshow("Depth", depthDisplay);
+ }
+
+keyboard = cv::waitKey(30);
+
+}
+
+delete zed;
 
 }
