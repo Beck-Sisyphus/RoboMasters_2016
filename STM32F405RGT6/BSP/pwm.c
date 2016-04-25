149e793be9fc6ebb2847b87f9022da1f995c4b96
Weiyou Dai <daiweiyou16@hotmail.com>
2016-04-25 11:41:55 -0700

    changed configuration

--- STM32F405RGT6/BSP/pwm.c
+++ STM32F405RGT6/BSP/pwm.c
@@ -14,24 +14,25 @@ void PWM_Configuration(void)
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA ,ENABLE);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
 
-    gpio.GPIO_Pin = GPIO_Pin_1|GPIO_Pin_0;
+    gpio.GPIO_Pin = GPIO_Pin_1|GPIO_Pin_0|GPIO_Pin_2;
     gpio.GPIO_Mode = GPIO_Mode_AF;
     gpio.GPIO_Speed = GPIO_Speed_100MHz;
     GPIO_Init(GPIOA,&gpio);
 
     GPIO_PinAFConfig(GPIOA,GPIO_PinSource1, GPIO_AF_TIM5);
     GPIO_PinAFConfig(GPIOA,GPIO_PinSource0,GPIO_AF_TIM5);    
-    
+		GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_TIM5);    
+
     tim.TIM_Prescaler = 84-1;
     tim.TIM_CounterMode = TIM_CounterMode_Up;
-    tim.TIM_Period = 2500;   //2.5ms
+    tim.TIM_Period = 2500;   // Sets period of 2.5ms
     tim.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBaseInit(TIM5,&tim);
     
     oc.TIM_OCMode = TIM_OCMode_PWM1;
     oc.TIM_OutputState = TIM_OutputState_Enable;
     oc.TIM_OutputNState = TIM_OutputState_Disable;
-    oc.TIM_Pulse = 1000;
+    oc.TIM_Pulse = 625; // output high 625 us per cycle
     oc.TIM_OCPolarity = TIM_OCPolarity_High;
     oc.TIM_OCNPolarity = TIM_OCPolarity_Low;
     oc.TIM_OCIdleState = TIM_OCIdleState_Reset;
