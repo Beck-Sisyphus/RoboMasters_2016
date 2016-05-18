833684c8694f5dd5a20d6f0106347a206ee74ee4
Beck-Sisyphus <pshfls@gmail.com>
2016-05-18 12:39:20 -0700

    Start refactoring PID code

--- STM32F405RGT6/User/main.h
+++ STM32F405RGT6/User/main.h
@@ -1,23 +1,22 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include "common.h"
 #include "stm32f4xx.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "bsp.h"
 #include "mpu6050_i2c.h"
 #include "mpu6050_interrupt.h"
 #include "mpu6050_driver.h"
 #include "mpu6050_process.h"
-//#include "hmc5883_driver.h"
 
 #include "app.h"
-#include "bsp.h"
 #include "module_rs232.h"
-#include "HAL_HMC5883L.h"
-#include "HMC5883L.h"
 #include "remote_control.h"
-
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include "pid.h"
+#include "pid_regulator.h"
 
 /*  Define the robot as
     0 Blue sample robot
