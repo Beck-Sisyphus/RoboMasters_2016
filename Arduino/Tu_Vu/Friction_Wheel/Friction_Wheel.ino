b042529b1cdbfbe6c00ce0169773b45f2a30ac48
Weiyou Dai <daiweiyou16@hotmail.com>
2016-05-17 23:53:41 -0700

    correct pin out for friction wheel

--- Arduino/Tu_Vu/Friction_Wheel/Friction_Wheel.ino
+++ Arduino/Tu_Vu/Friction_Wheel/Friction_Wheel.ino
@@ -1,16 +1,32 @@
-int8_t pin = 9;
+int8_t front_left_pin = 9; 
+int8_t front_right_pin = 8;
+int8_t rear_left_pin = 7;
+int8_t rear_right_pin = 6;
 
 void setup() {
-  pinMode(pin, OUTPUT);
+  pinMode(front_left_pin, OUTPUT);
+  pinMode(front_right_pin, OUTPUT);
+  pinMode(rear_left_pin, OUTPUT);
+  pinMode(rear_right_pin, OUTPUT);
+
 
   // initializes friction wheels 
   // 40% duty cycle
   // equivalent to setting PWMx = 1000 in trapezoid board
-  analogWrite(pin, 255 * 0.4);
+  analogWrite(front_left_pin, 255 * 0.4);
+  analogWrite(front_right_pin, 255 * 0.4);
+  analogWrite(rear_left_pin, 255 * 0.4);
+  analogWrite(rear_right_pin, 255 * 0.4);
+
   delay(500);
 }
 
 void loop() {
   // equivalent to setting PWMx = 1500 in trapezoid board
-  analogWrite(pin, 255 * 0.6);
+  analogWrite(front_left_pin, 255 * 0.6);
+  analogWrite(front_right_pin, 255 * 0.6);
+  analogWrite(rear_left_pin, 255 * 0.6);
+  analogWrite(rear_right_pin, 255 * 0.6);
+
 }
+
