262e068b1c76b70942e78a21a2594fe5b6639704
Beck-Sisyphus <pshfls@gmail.com>
2016-05-24 13:02:52 -0700

    Reimplementing the PID can build the state machine in control task

---
+++ STM32F405RGT6/APP/ControlTask.c
@@ -0,0 +1,219 @@
+#include "main.h"
+
+PID_Regulator_t GMPPositionPID = GIMBAL_MOTOR_PITCH_POSITION_PID_DEFAULT;
+PID_Regulator_t GMPSpeedPID = GIMBAL_MOTOR_PITCH_SPEED_PID_DEFAULT;
+PID_Regulator_t GMYPositionPID = GIMBAL_MOTOR_YAW_POSITION_PID_DEFAULT;
+PID_Regulator_t GMYSpeedPID = GIMBAL_MOTOR_YAW_SPEED_PID_DEFAULT;
+
+/*--------------------------------------------CTRL Variables----------------------------------------*/
+WorkState_e lastWorkState = PREPARE_STATE;
+WorkState_e workState = PREPARE_STATE;
+
+static uint32_t time_tick_1ms = 0;
+/*
+*********************************************************************************************************
+*                                            FUNCTIONS
+*********************************************************************************************************
+*/
+
+static void SetWorkState(WorkState_e state)
+{
+    workState = state;
+}
+
+WorkState_e GetWorkState()
+{
+	return workState;
+}
+
+//控制任务，放在timer6 1ms定时中断中执行
+// Control task running at timer 6 interrupt with 1ms
+void Control_Task(void)
+{
+	time_tick_1ms++;
+	WorkStateFSM();
+	WorkStateSwitchProcess();
+	// //启动后根据磁力计的数据初始化四元数
+  // // Initialize quaternion coordinate from magnetometer
+	// if(time_tick_1ms <100) { Init_Quaternion(); }
+	// //平台稳定启动后，复位陀螺仪模块
+  // // Reset the gyroscope after the gimbal is stablized
+	// if(time_tick_1ms == PREPARE_TIME_TICK_MS/2) { GYRO_RST(); }
+
+	// Gimbal control
+  //模式切换处理，得到位置环的设定值和给定值
+  // Mode switch method, to get the target and measured value of position loop
+	GimbalYawControlModeSwitch();
+  GMPitchControlLoop();
+	GMYawControlLoop();
+	SetGimbalMotorOutput();
+	//chassis motor control
+	if(time_tick_1ms%4 == 0)         //motor control frequency 4ms
+	{
+		// Supervise task //监控任务
+		SuperviseTask();
+		// Chassis control task//底盘控制任务
+		CMControlLoop();
+    // Shooting mechanism control task//发射机构控制任务
+		ShooterMControlLoop();
+	}
+}
+
+
+void WorkStateFSM(void)
+{
+	lastWorkState = workState;
+	switch(workState)
+	{
+		case PREPARE_STATE:
+		{
+			if(GetInputMode() == STOP || Is_Serious_Error())
+			{
+				workState = STOP_STATE;
+			}
+			else if(time_tick_1ms > PREPARE_TIME_TICK_MS)
+			{
+				workState = SEMI_AUTONOMOUS_STATE;
+			}
+		}break;
+		case SEMI_AUTONOMOUS_STATE:
+		{
+			if(GetInputMode() == STOP || Is_Serious_Error())
+			{
+				workState = STOP_STATE;
+			}
+			else if((!IsRemoteBeingAction() ||(Get_Lost_Error(LOST_ERROR_RC) == LOST_ERROR_RC)) && GetShootState() != SHOOTING)
+			{
+				workState = STANDBY_STATE;
+			}
+		}break;
+    case MANUAL_STATE:
+    {
+      if(GetInputMode() == STOP || Is_Serious_Error())
+      {
+        workState = STOP_STATE;
+      }
+    }break;
+		case STANDBY_STATE:
+		{
+			if(GetInputMode() == STOP || Is_Serious_Error())
+			{
+				workState = STOP_STATE;
+			}
+			else if(IsRemoteBeingAction() || (GetShootState()==SHOOTING) || GetFrictionState() == FRICTION_WHEEL_START_TURNNING)
+			{
+				workState = SEMI_AUTONOMOUS_STATE;
+			}
+		}break;
+		case STOP_STATE:
+		{
+			if(GetInputMode() != STOP && !Is_Serious_Error())
+			{
+				workState = PREPARE_STATE;
+			}
+		}break;
+		default:
+		{
+
+		}
+	}
+}
+
+static void WorkStateSwitchProcess(void)
+{
+	//如果从其他模式切换到prepare模式，要将一系列参数初始化
+  // If switch from other state to prepare state, initialize all parameters
+	if((lastWorkState != workState) && (workState == PREPARE_STATE))
+	{
+		ControtLoopTaskInit();
+		RemoteTaskInit();
+	}
+}
+
+// Pitch axis double loop control
+void GMPitchControlLoop(void)
+{
+	GMPPositionPID.kp = PITCH_POSITION_KP_DEFAULTS + PitchPositionSavedPID.kp_offset;
+	GMPPositionPID.ki = PITCH_POSITION_KI_DEFAULTS + PitchPositionSavedPID.ki_offset;
+	GMPPositionPID.kd = PITCH_POSITION_KD_DEFAULTS + PitchPositionSavedPID.kd_offset;
+
+	GMPSpeedPID.kp = PITCH_SPEED_KP_DEFAULTS + PitchSpeedSavedPID.kp_offset;
+	GMPSpeedPID.ki = PITCH_SPEED_KI_DEFAULTS + PitchSpeedSavedPID.ki_offset;
+	GMPSpeedPID.kd = PITCH_SPEED_KD_DEFAULTS + PitchSpeedSavedPID.kd_offset;
+
+	GMPPositionPID.ref = GimbalRef.pitch_angle_dynamic_ref;
+	// GMPPositionPID.fdb = -GMPitchEncoder.ecd_angle * GMPitchRamp.Calc(&GMPitchRamp);    // With ramp function
+  GMPPositionPID.fdb = -GMPitchEncoder.ecd_angle; // Without ramp function
+	GMPPositionPID.Calc(&GMPPositionPID);   // Pitch axis position control loop
+	//pitch speed control
+	GMPSpeedPID.ref = GMPPositionPID.output;
+	GMPSpeedPID.fdb = MPU6050_Real_Data.Gyro_Y;
+	GMPSpeedPID.Calc(&GMPSpeedPID);
+}
+
+void GMYawControlLoop(void)
+{
+	GMYPositionPID.kp = YAW_POSITION_KP_DEFAULTS + YawPositionSavedPID.kp_offset;//  gAppParamStruct.YawPositionPID.kp_offset;  //may be bug if more operation  done
+	GMYPositionPID.ki = YAW_POSITION_KI_DEFAULTS + YawPositionSavedPID.ki_offset;
+	GMYPositionPID.kd = YAW_POSITION_KD_DEFAULTS + YawPositionSavedPID.kd_offset;
+
+	GMYSpeedPID.kp = YAW_SPEED_KP_DEFAULTS + YawSpeedSavedPID.kp_offset;
+	GMYSpeedPID.ki = YAW_SPEED_KI_DEFAULTS + YawSpeedSavedPID.ki_offset;
+	GMYSpeedPID.kd = YAW_SPEED_KD_DEFAULTS + YawSpeedSavedPID.kd_offset;
+
+  GMYPositionPID.ref = GimbalRef.yaw_angle_dynamic_ref;
+  GMYPositionPID.fdb = -GMYawEncoder.ecd_angle;
+	GMYPositionPID.Calc(&GMYPositionPID);
+	//yaw speed control
+	GMYSpeedPID.ref = GMYPositionPID.output;
+	GMYSpeedPID.fdb = MPU6050_Real_Data.Gyro_Z;
+	GMYSpeedPID.Calc(&GMYSpeedPID);
+}
+
+// Control Task initialization //控制任务初始化程序
+void ControtLoopTaskInit(void)
+{
+	// Clear the timing  //计数初始化
+	time_tick_1ms = 0;   // Clear the count in the interrupt //中断中的计数清零
+	// Parameter initialization	//程序参数初始化
+	AppParamInit();
+	// initialize parameter deviation after calibration	//校准后参数偏差值初始化
+	Sensor_Offset_Param_Init(&gAppParamStruct);
+	// Set the work state	//设置工作模式
+	SetWorkState(PREPARE_STATE);
+
+	// // Ramp function initialization	//斜坡初始化
+	// GMPitchRamp.SetScale(&GMPitchRamp, PREPARE_TIME_TICK_MS);
+	// GMYawRamp.SetScale(&GMYawRamp, PREPARE_TIME_TICK_MS);
+	// GMPitchRamp.ResetCounter(&GMPitchRamp);
+	// GMYawRamp.ResetCounter(&GMYawRamp);
+
+	// Initialize the given angle for gimbal	//云台给定角度初始化
+	GimbalRef.pitch_angle_dynamic_ref = 0.0f;
+	GimbalRef.yaw_angle_dynamic_ref = 0.0f;
+
+  // // monitoring task initialization //监控任务初始化
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_RC));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_IMU));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_ZGYRO));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_MOTOR1));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_MOTOR2));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_MOTOR3));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_MOTOR4));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_MOTOR5));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_MOTOR6));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_DEADLOCK));
+  // LostCounterFeed(GetLostCounter(LOST_COUNTER_INDEX_NOCALI));
+
+	// // PID initialization
+	// ShootMotorSpeedPID.Reset(&ShootMotorSpeedPID);
+	GMPPositionPID.Reset(&GMPPositionPID);
+	GMPSpeedPID.Reset(&GMPSpeedPID);
+	GMYPositionPID.Reset(&GMYPositionPID);
+	GMYSpeedPID.Reset(&GMYSpeedPID);
+	// CMRotatePID.Reset(&CMRotatePID);
+	// CM1SpeedPID.Reset(&CM1SpeedPID);
+	// CM2SpeedPID.Reset(&CM2SpeedPID);
+	// CM3SpeedPID.Reset(&CM3SpeedPID);
+	// CM4SpeedPID.Reset(&CM4SpeedPID);
+}
---
+++ STM32F405RGT6/APP/ControlTask.h
@@ -0,0 +1,113 @@
+#ifndef _CONTROL_TASK_H_
+#define _CONTROL_TASK_H_
+#include "main.h"
+
+//initialize status:
+typedef enum
+{
+    PREPARE_STATE,      // The initialization state when power turned on
+    STANDBY_STATE,      // The state when gimbal stop moving
+    MANUAL_STATE,       // For manual control the robot
+    SEMI_AUTONOMOUS_STATE,  // Allow the computer vision to control the gimbal
+    FULL_AUTONOMOUS_STATE,  // Let the SLAM to control the chassis for navigate
+    STOP_STATE,        	// The state without movement
+}WorkState_e;
+
+#define GIMBAL_MOTOR_PITCH_POSITION_PID_DEFAULT \
+{\
+	0,\
+	0,\
+	{0,0},\
+	PITCH_POSITION_KP_DEFAULTS,\
+	PITCH_POSITION_KI_DEFAULTS,\
+	PITCH_POSITION_KD_DEFAULTS,\
+	0,\
+	0,\
+	0,\
+	4900,\
+	1000,\
+	1500,\
+	0,\
+	4900,\
+	0,\
+	0,\
+	0,\
+	&PID_Calc,\
+	&PID_Reset,\
+}\
+
+#define GIMBAL_MOTOR_PITCH_SPEED_PID_DEFAULT \
+{\
+	0,\
+	0,\
+	{0,0},\
+	PITCH_SPEED_KP_DEFAULTS,\
+	PITCH_SPEED_KI_DEFAULTS,\
+	PITCH_SPEED_KD_DEFAULTS,\
+	0,\
+	0,\
+	0,\
+	4900,\
+	1000,\
+	1500,\
+	0,\
+	4900,\
+	0,\
+	0,\
+	0,\
+	&PID_Calc,\
+	&PID_Reset,\
+}\
+
+#define GIMBAL_MOTOR_YAW_POSITION_PID_DEFAULT \
+{\
+	0,\
+	0,\
+	{0,0},\
+	YAW_POSITION_KP_DEFAULTS,\
+	YAW_POSITION_KI_DEFAULTS,\
+	YAW_POSITION_KD_DEFAULTS,\
+	0,\
+	0,\
+	0,\
+	4900,\
+	1000,\
+	1500,\
+	0,\
+	5000,\
+	0,\
+	0,\
+	0,\
+	&PID_Calc,\
+	&PID_Reset,\
+}\
+
+#define GIMBAL_MOTOR_YAW_SPEED_PID_DEFAULT \
+{\
+	0,\
+	0,\
+	{0,0},\
+	YAW_SPEED_KP_DEFAULTS,\
+	YAW_SPEED_KI_DEFAULTS,\
+	YAW_SPEED_KD_DEFAULTS,\
+	0,\
+	0,\
+	0,\
+	4900,\
+	1000,\
+	1500,\
+	0,\
+	4900,\
+	0,\
+	0,\
+	0,\
+	&PID_Calc,\
+	&PID_Reset,\
+}\
+
+extern PID_Regulator_t GMPPositionPID;      //pitch axis position PID loop
+extern PID_Regulator_t GMPSpeedPID;      		//pitch axis velocity PID loop
+extern PID_Regulator_t GMYPositionPID;			//yaw axis position PID loop
+extern PID_Regulator_t GMYSpeedPID;      		//yaw axis velocity PID loop
+
+#endif
