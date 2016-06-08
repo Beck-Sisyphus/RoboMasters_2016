8d2802d9dd02c84f1de538ea6f8bf841709e23ca
Beck-Sisyphus <pshfls@gmail.com>
2016-06-08 08:17:45 -0700

    using bothing driving and shooting

--- STM32F405RGT6/User/remote_control.c
+++ STM32F405RGT6/User/remote_control.c
@@ -43,11 +43,16 @@ void Remote_Control() {
             yaw_Position = data_usart_3.packet.yaw_req;
             manual_Control_Turret = 0;
         } else if(RC_Ctl.rc.s1 == RC_SW_DOWN && RC_Ctl.rc.s2 == RC_SW_DOWN) {
-            pitch = (RC_Ctl.rc.ch3 - RC_CH_VALUE_OFFSET) / 100;
-            yaw = (RC_Ctl.rc.ch2 - RC_CH_VALUE_OFFSET) / 100;
+            wheel_control(drive, strafe, 0);
+            pitch = (RC_Ctl.rc.ch1 - RC_CH_VALUE_OFFSET) / 100;
+            yaw = (RC_Ctl.rc.ch0 - RC_CH_VALUE_OFFSET) / 100;
             manual_Control_Turret = 1;
             pitch_Position += pitch;
+            pitch_Position = min(pitch_Position, REAL_CANNON_PITCH_HIGH);
+            pitch_Position = max(pitch_Position, REAL_CANNON_PITCH_LOW);
             yaw_Position += yaw;
+            yaw_Position = min(yaw_Position, REAL_CANNON_YAW_HIGH);
+            yaw_Position = max(yaw_Position, REAL_CANNON_YAW_LOW);
         } else if (RC_Ctl.rc.s2 == RC_SW_MID) {
           /* code */
         } else {
