83d429215b56e1d8b9dbe8a2bbdc69b4592de51d
Yufang Sun <mandary@users.noreply.github.com>
2016-06-26 00:45:57 +0000

    script to random sample train/val data

---
+++ Computer_Vision/data/resample.py
@@ -0,0 +1,24 @@
+from os import getcwd
+import glob
+import random
+import pdb
+
+TRAIN = 0.5
+
+def resample():
+	pdb.set_trace()
+	wd = getcwd()
+	train = open("train.txt", 'w')
+	val = open("val.txt", 'w')
+	pics = glob.glob("positive/*.jpg")
+	index = range(len(pics))
+	random.shuffle(index)
+	size = int(len(pics) * 0.5)
+	for i in range(size):
+		train.write(wd + pics[index[i]] + "\n")
+	train.close()
+	for i in range(size, len(pics)):
+		val.write(wd + pics[index[i]] + "\n")
+	val.close()
+
+resample()
