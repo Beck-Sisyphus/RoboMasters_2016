fa661d843e5158332259c07fc3036fc0357b3b60
NATAT PREMVUTI <natatp@uw.edu>
2016-07-13 21:01:01 -0700

    32 bit packet on usart3 and clear dma on arduino reset

--- STM32F405RGT6/BSP/usart3.c
+++ STM32F405RGT6/BSP/usart3.c
@@ -4,13 +4,9 @@
 // #include "usart1.h" // ONLY for the RC_Ctl_t, remove this when we use diff. type
 #include "led.h"
 
-
-
-
-volatile unsigned char arduino_rx_buffer_usart_3[16];
+volatile unsigned char arduino_rx_buffer_usart_3[32];
 DMA_InitTypeDef dma_usart_3;
 volatile arduino_data data_usart_3;
-int16_t testData;
 
 /*-----USART3_TX-----PB10---*/
 /*-----USART3_RX-----PB11---*/
@@ -61,7 +57,7 @@ void USART3_Configuration(void)
     dma_usart_3.DMA_PeripheralBaseAddr = (uint32_t)&(USART3->DR);
     dma_usart_3.DMA_Memory0BaseAddr = (uint32_t)arduino_rx_buffer_usart_3;
     dma_usart_3.DMA_DIR = DMA_DIR_PeripheralToMemory;
-    dma_usart_3.DMA_BufferSize = 16;
+    dma_usart_3.DMA_BufferSize = 32;
     dma_usart_3.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
     dma_usart_3.DMA_MemoryInc = DMA_MemoryInc_Enable;
     dma_usart_3.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
@@ -83,7 +79,6 @@ void USART3_SendChar(unsigned char b)
     USART_SendData(USART3,b);
 }
 
-
 int fputc(int ch, FILE *f)
 {
     while (USART_GetFlagStatus(USART3,USART_FLAG_TC) == RESET);
@@ -100,6 +95,22 @@ void USART3_IRQHandler(void)
     }
 }
 
+/*
+0  1  header
+2     feeder_motor_state
+3     friction_motor_state
+4  5  pitch_req
+6  7  yaw_req
+8  9  feeder_motor_pwm
+10 11 friction_motor_pwm
+12 13 drive_req
+14 15 strafe_req
+16 17 rotate_req
+18 19 
+20 21 mpu_roll
+22 23 mpu_pitch
+24 25 mpu_yaw
+*/
 void DMA1_Stream1_IRQHandler(void)
 {
     if(DMA_GetITStatus(DMA1_Stream1, DMA_IT_TCIF1)) // DMA_IT_TCI_F1 because we are clearing stream 1
@@ -107,16 +118,36 @@ void DMA1_Stream1_IRQHandler(void)
         DMA_ClearFlag(DMA1_Stream1, DMA_FLAG_TCIF1);
         DMA_ClearITPendingBit(DMA1_Stream1, DMA_IT_TCIF1);
 
-        data_usart_3.packet.header = (((int16_t) arduino_rx_buffer_usart_3[0] << 8)) | (arduino_rx_buffer_usart_3[1] & 255);
-        data_usart_3.packet.feeder_motor_state = arduino_rx_buffer_usart_3[2] & 255;
-        data_usart_3.packet.friction_motor_state = arduino_rx_buffer_usart_3[3] & 255;
-        data_usart_3.packet.pitch_req = (((int16_t) arduino_rx_buffer_usart_3[4] << 8)) | (arduino_rx_buffer_usart_3[5] & 255);
-        data_usart_3.packet.yaw_req = (((int16_t) arduino_rx_buffer_usart_3[6] << 8)) | (arduino_rx_buffer_usart_3[7] & 255);
-        data_usart_3.packet.feeder_motor_pwm = (((int16_t) arduino_rx_buffer_usart_3[8] << 8)) | (arduino_rx_buffer_usart_3[9] & 255);
-        data_usart_3.packet.friction_motor_pwm = (((int16_t) arduino_rx_buffer_usart_3[10] << 8)) | (arduino_rx_buffer_usart_3[11] & 255);
-        LED1_TOGGLE();
+        // reset dma if header is not right
+        if (arduino_rx_buffer_usart_3[0] ^ 0xFA) {
+            // restart the dma
+            DMA_ClearFlag(DMA1_Stream1, DMA_FLAG_FEIF1|DMA_FLAG_DMEIF1|DMA_FLAG_TEIF1|DMA_FLAG_HTIF1|DMA_FLAG_TCIF1);
+            DMA_Cmd(DMA1_Stream1, DISABLE);
+            while (DMA1_Stream1->CR & DMA_SxCR_EN);
+            DMA_Cmd(DMA1_Stream1, ENABLE);
+        } else {
+            data_usart_3.packet.header = (((int16_t) arduino_rx_buffer_usart_3[0] << 8)) | (arduino_rx_buffer_usart_3[1] & 255);
+            data_usart_3.packet.feeder_motor_state = arduino_rx_buffer_usart_3[2] & 255;
+            data_usart_3.packet.friction_motor_state = arduino_rx_buffer_usart_3[3] & 255;
+            data_usart_3.packet.pitch_req = (((int16_t) arduino_rx_buffer_usart_3[4] << 8)) | (arduino_rx_buffer_usart_3[5] & 255);
+            data_usart_3.packet.yaw_req = (((int16_t) arduino_rx_buffer_usart_3[6] << 8)) | (arduino_rx_buffer_usart_3[7] & 255);
+            data_usart_3.packet.feeder_motor_pwm = (((int16_t) arduino_rx_buffer_usart_3[8] << 8)) | (arduino_rx_buffer_usart_3[9] & 255);
+            data_usart_3.packet.friction_motor_pwm = (((int16_t) arduino_rx_buffer_usart_3[10] << 8)) | (arduino_rx_buffer_usart_3[11] & 255);
 
+            data_usart_3.packet.drive_req = (((int16_t) arduino_rx_buffer_usart_3[12] << 8)) | (arduino_rx_buffer_usart_3[13] & 255);
+            data_usart_3.packet.strafe_req = (((int16_t) arduino_rx_buffer_usart_3[14] << 8)) | (arduino_rx_buffer_usart_3[15] & 255);
+            data_usart_3.packet.rotate_req = (((int16_t) arduino_rx_buffer_usart_3[16] << 8)) | (arduino_rx_buffer_usart_3[17] & 255);
 
+            data_usart_3.packet.mpu_x = (((int16_t) arduino_rx_buffer_usart_3[20] << 8)) | (arduino_rx_buffer_usart_3[21] & 255);
+            data_usart_3.packet.mpu_y = (((int16_t) arduino_rx_buffer_usart_3[22] << 8)) | (arduino_rx_buffer_usart_3[23] & 255);
+            data_usart_3.packet.mpu_z = (((int16_t) arduino_rx_buffer_usart_3[24] << 8)) | (arduino_rx_buffer_usart_3[25] & 255);
 
+            LED1_TOGGLE();
+            if (data_usart_3.packet.feeder_motor_state) {
+                LED2_ON();
+            } else {
+                LED2_OFF();
+            }
+        }
     }
 }
--- STM32F405RGT6/BSP/usart3.h
+++ STM32F405RGT6/BSP/usart3.h
@@ -4,48 +4,6 @@
 #include <stm32f4xx.h>
 #include <stdio.h>
 
-// /**** rc channel definition ****/
-// #define RC_CH_VALUE_MIN                 ((uint16_t)364 )
-// #define RC_CH_VALUE_OFFSET              ((uint16_t)1024)
-// #define RC_CH_VALUE_MAX                 ((uint16_t)1684)
-
-// /**** rc switch definition ****/
-// #define RC_SW_UP                        ((uint16_t)1)
-// #define RC_SW_MID                       ((uint16_t)3)
-// #define RC_SW_DOWN                      ((uint16_t)2)
-
-// /**** pc key definition ****/
-// #define KEY_PRESSED_OFFSET_W            ((uint16_t)0x01<<0)
-// #define KEY_PRESSED_OFFSET_S            ((uint16_t)0x01<<1)
-// #define KEY_PRESSED_OFFSET_A            ((uint16_t)0x01<<2)
-// #define KEY_PRESSED_OFFSET_D            ((uint16_t)0x01<<3)
-// #define KEY_PRESSED_OFFSET_Q            ((uint16_t)0x01<<4)
-// #define KEY_PRESSED_OFFSET_E            ((uint16_t)0x01<<5)
-// #define KEY_PRESSED_OFFSET_SHIFT        ((uint16_t)0x01<<6)
-// #define KEY_PRESSED_OFFSET_CTRL         ((uint16_t)0x01<<7)
-
-// /**** data struct ****/
-// typedef struct {
-//     struct { 
-//         uint16_t ch0;
-//         uint16_t ch1;
-//         uint16_t ch2;
-//         uint16_t ch3;
-//         uint8_t s1;
-//         uint8_t s2;
-//     } rc;
-//     struct {
-//         int16_t x;
-//         int16_t y;
-//         int16_t z;
-//         uint8_t press_l;
-//         uint8_t press_r;
-//     } mouse;
-//     struct {
-//         uint16_t v;
-//     } key;
-// } RC_Ctl_t;
-
 typedef struct {
     struct { 
         int16_t header;
@@ -55,6 +13,12 @@ typedef struct {
         int16_t yaw_req;
         int16_t feeder_motor_pwm;
         int16_t friction_motor_pwm;
+        int16_t drive_req;
+        int16_t strafe_req;
+        int16_t rotate_req;
+        int16_t mpu_x;
+        int16_t mpu_y;
+        int16_t mpu_z;
     } packet;
 } arduino_data;
 
