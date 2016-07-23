3f5496eda5d5806f1c9ecc6e6d82c97f1c96cad1
Beck <pshfls@gmail.com>
2016-07-24 07:46:10 +0800

    Used for debugging

--- Arduino/Tu_Vu/Serial_RX/Serial_RX.ino
+++ Arduino/Tu_Vu/Serial_RX/Serial_RX.ino
@@ -5,19 +5,26 @@ String msg;
 //Serial for: serial monitor window to show data 
 void setup() {
  Serial.begin(115200);
- Serial3.begin(115200);
+ Serial1.begin(115200);
 }
 
 // reads input as strings
 // need char manipulation to cut away
 // extra, wrong bits in message
 void loop() {
-  if(Serial3.available() > 0) {
-    msg = Serial3.readString();
+  if(Serial.available() > 0) {
+    msg = Serial.readString();
     for(int i = 0; i < msg.length(); i++) {
       msg.setCharAt(i, msg.charAt(i) & 127);
     }
   Serial.println(msg);
+  }
+    if(Serial1.available() > 0) {
+    msg = Serial1.readString();
+    for(int i = 0; i < msg.length(); i++) {
+      msg.setCharAt(i, msg.charAt(i) & 127);
+    }
+    Serial.println(msg);
   }
 }
 
